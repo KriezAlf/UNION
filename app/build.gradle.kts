6d551bb6879d06058a7fe3a62a6f4ed2b6f7b910
STEVE <ignatius.steven03@gmail.com>
2024-10-23 15:43:26 +0700

    firebase edit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,11 +39,13 @@ android {
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.5.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-firestore")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
